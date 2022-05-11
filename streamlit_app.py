8a6cf9aaf21b029b4e8e8619f056104c158c6bcc
Varvara Kozhukhova <53300653+vkozhukhova@users.noreply.github.com>
2022-05-11 15:34:34 +0300

    smilees

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title("My parents new healthy diner")
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')             
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
